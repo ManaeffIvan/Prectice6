3ca222a6b7b7c964531926e64e61fb7187dc37fb
ManaeffIvan <manaeff.ivan@gmail.com>
2026-03-23 14:47:58 +0500

    add second

--- SortK.cpp
+++ SortK.cpp
@@ -44,3 +44,84 @@ int main() {
 	delete[] a;
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string clean_str(string s) {
+	string ans = "", s1 = "";
+	int i = 0;
+	while (i < s.length() && (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')) {
+		i++;
+	}
+	for (; i < s.length(); i++) {
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n') {
+			s1 += s[i];
+		}
+		else {
+			ans += s1 + s[i];
+			s1 = "";
+		}
+	}
+	return ans;
+}
+
+bool check_is_fp(string s) {
+	s = clean_str(s);
+	int cnt_sign = 0;
+	bool is_end = false, first_simb = true, before_point = false, before_e = false;
+	
+	for (int i = 0; i < s.length(); i++) {
+		if (s[i] == '.') {
+			if (before_point) {
+				return false;
+			}
+			before_point = true;
+		}
+		else if (s[i] == '+' || s[i] == '-') {
+			cnt_sign++;
+			if (!first_simb && s[i - 1] != 'e' && s[i - 1] != 'E') {
+				return false;
+			}
+		}
+		else if (s[i] == 'e' || s[i] == 'E') {
+			if (first_simb || before_e) {
+				return false;
+			}
+			before_e = true;
+		}
+		else if (int(s[i]) < int('0') || int(s[i]) > int('9')) {
+			return false;
+		}
+		else {
+			first_simb = false;
+		}
+		
+	}
+	
+	return true;
+}
+
+int main() {
+	string s;
+	cin >> s;
+
+	cout << s << endl;
+	cout << (check_is_fp(s)? "" : "not ") << "a floating-point number" << endl;
+
+	cout << endl;
+	
+    
+	return 0;
+}
